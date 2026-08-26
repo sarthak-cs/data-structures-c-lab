92f355b36c9561a0d324c210f70a87a57a50c2aa
Sarthak Tyagi <153098175+sarthak-cs@users.noreply.github.com>
2026-08-26 12:34:11 +0530

    Implement 2D array addition in C

---
+++ 2D-array-add.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void main(){
+    int r, c;
+
+    printf("Enter rows and columns: ");
+    scanf("%d %d", &r, &c);
+    int a[r][c], b[r][c], res[r][c];
+
+    printf("Enter Matrix A:\n");
+
+    for (int i = 0; i < r; i++)
+        for (int j = 0; j < c; j++)
+            scanf("%d", &a[i][j]);
+
+    printf("Enter Matrix B:\n");
+
+    for (int i = 0; i < r; i++)
+        for (int j = 0; j < c; j++)
+            scanf("%d", &b[i][j]);
+
+    for (int i = 0; i < r; i++)
+        for (int j = 0; j < c; j++)
+            res[i][j] = a[i][j] + b[i][j];
+
+    printf("Sum:\n");
+
+    for (int i = 0; i < r; i++){
+        for (int j = 0; j < c; j++)
+            printf("%d ", res[i][j]);
+
+        printf("\n");
+    }
+}
